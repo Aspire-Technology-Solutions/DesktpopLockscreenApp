74eced2700d2fcd2c97ecbebb08d10bd7c24c7d1
Warren Sherwen <warren.sherwen@aspirets.com>
2024-11-20 20:30:45 +0000

    Update branding.ps1

--- branding.ps1
+++ branding.ps1
@@ -1,130 +1,106 @@
 Param(
-[Parameter(Mandatory=$true)]
-[ValidateSet("Install", "Uninstall")]
-[String[]]
-$Mode
+    [Parameter(Mandatory=$true)]
+	[ValidateSet("Install", "Uninstall")]
+	[String[]]
+	$Mode
 )
 # Author: Warren Sherwen
 # Verison: 1.0
 
 # Defines the log file location.
-$Logfile = "$env:windir\Temp\Logs\lockscreenandbackground.log"
+$Logfile = "$env:windir\Temp\Logs\companybranding.log"
 
 # LogWrite Function.
 Function LogWrite{
-   Param ([string]$logstring)
-   Add-content $Logfile -value $logstring
-   write-output $logstring
+    Param ([string]$logstring)
+    Add-content $Logfile -value $logstring
+    write-output $logstring
 }
 
 function Get-TimeStamp {
 	return "[{0:dd/MM/yy} {0:HH:mm:ss}]" -f (Get-Date)
 }
 
-if (!(Test-Path "$env:windir\Temp\Logs\"))
-	{
+if (!(Test-Path "$env:windir\Temp\Logs\")) {
 	   mkdir $env:windir\Temp\Logs
-	   LogWrite "$(Get-TimeStamp): Lockscreen and Backgruoud script has started."
+	   LogWrite "$(Get-TimeStamp): Company Branding script has started."
 	   LogWrite "$(Get-TimeStamp): Log directory created."
-	}
-	else
-	{
-		LogWrite "$(Get-TimeStamp): Lockscreen and Backgruoud script has started."
+	} else {
+		LogWrite "$(Get-TimeStamp): Company Branding script has started."
 		LogWrite "$(Get-TimeStamp): Log directory exists."
 	}
 
-if (!(Test-Path "$Env:SystemDrive\Background"))
-	{
-       LogWrite "$(Get-TimeStamp): Creating background folder."
-       mkdir $Env:SystemDrive\Background
-       LogWrite "$(Get-TimeStamp): Background folder created."
-	}
-	else
-	{
-        LogWrite "$(Get-TimeStamp): Background folder already exists."
-	}
+$WallpaperIMG = "Background.jpg"
+$LockscreenIMG = "Lockscreen.jpg"
 
-
-#Registry key paths
 $RegistryKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
-
-$DesktopImagePath = "DesktopImagePath"
-$DesktopImageStatus = "DesktopImageStatus"
-$DesktopImageUrl = "DesktopImageUrl"
-
-
-$LockScreenImagePath = "LockScreenImagePath"
-$LockScreenImageStatus = "LockScreenImageStatus"
-$LockScreenImageUrl = "LockScreenImageUrl"
-
-#Status value for enforcing background and lockscreen.
+$BackgroundPath = "DesktopImagePath"
+$BackgroundStatus = "DesktopImageStatus"
+$DesktopUrl = "DesktopImageUrl"
+$LockScreenPath = "LockScreenImagePath"
+$LockScreenStatus = "LockScreenImageStatus"
+$LockScreenUrl = "LockScreenImageUrl"
 $StatusValue = "1"
 
-####################################### ---- Amendendable Variables ---- #######################################
+$BackgroundLocation = "C:\Background\Desktop.jpg"
+$LockscreenLocation = "C:\Background\Lockscreen.jpg"
 
-#Where your saved files in the assets folder may differ from background or lockscreen.
-$BackgroundImage = "Background.jpg"
-$LockscreenImage = "Lockscreen.jpg"
+If ($Mode -eq "Install") {
+    if (!(Test-Path "c:\Background")) {
+        LogWrite "$(Get-TimeStamp): Creating background folder."
+	    mkdir C:\Background -Force
+	}else{
+        LogWrite "$(Get-TimeStamp): Background folder exists."
+	}
 
-# The below file names, only need amending where you plan to have diffrent types of images per user/device type. I.e. several deployed applications.
-$NewBackgroundName = "Background.jpg"
-$NewLockScreenName = "Lockscreen.jpg"
+    New-Item -Path $RegistryKeyPath -Force -ErrorAction SilentlyContinue
 
-####################################### ---- Amendendable Variables ---- #######################################
-
-$DesktopBackgroundLocalFile = "$Env:SystemDrive\Background\$NewBackgroundName"
-$LockscreenBackgroundLocalFile = "$Env:SystemDrive\Background\$NewLockScreenName"
-
-	If ($Mode -eq "Install") {
-        if (!$LockscreenImage -and !$BackgroundImage){
-            LogWrite "$(Get-TimeStamp): You must have either a Lockscreen or Wallpaper image defined, witin the assets folder."
-            LogWrite "$(Get-TimeStamp): Setup is exiting."
-            Exit
-        }
-        else{
-            if(!(Test-Path $RegistryKeyPath)){
-                LogWrite "$(Get-TimeStamp):Creating registry path: $($RegistryKeyPath)."
-                New-Item -Path $RegistryKeyPath -Force
-            }
-
-            if ($LockscreenImage){
-                LogWrite "$(Get-TimeStamp): Copy lockscreen ""$($LockscreenImage)"" to ""$($LockscreenBackgroundLocalFile)"""
-                Copy-Item ".\Assets\$LockscreenImage" $LockscreenBackgroundLocalFile -Force
-                LogWrite "$(Get-TimeStamp):Creating regkeys for lockscreen"
-                New-ItemProperty -Path $RegistryKeyPath -Name $LockScreenImageStatus -Value $StatusValue -PropertyType DWORD -Force
-                New-ItemProperty -Path $RegistryKeyPath -Name $LockScreenImagePath -Value $LockscreenBackgroundLocalFile -PropertyType STRING -Force
-                New-ItemProperty -Path $RegistryKeyPath -Name $LockScreenImageUrl -Value $LockscreenBackgroundLocalFile -PropertyType STRING -Force
-                LogWrite "$(Get-TimeStamp): Registry files created"
-            }
-
-            if ($BackgroundImage){
-                LogWrite "$(Get-TimeStamp): Copy wallpaper ""$($BackgroundImage)"" to ""$($DesktopBackgroundLocalFile)"""
-                Copy-Item ".\Assets\$BackgroundImage" $DesktopBackgroundLocalFile -Force
-                LogWrite "$(Get-TimeStamp):Creating regkeys for wallpaper"
-                New-ItemProperty -Path $RegistryKeyPath -Name $DesktopImageStatus -value $StatusValue -PropertyType DWORD -Force
-                New-ItemProperty -Path $RegistryKeyPath -Name $DesktopImagePath -value $DesktopBackgroundLocalFile -PropertyType STRING -Force
-                New-ItemProperty -Path $RegistryKeyPath -Name $DesktopImageUrl -value $DesktopBackgroundLocalFile -PropertyType STRING -Force
-                LogWrite "$(Get-TimeStamp): Registry files created"
-            }  
-        }
-        LogWrite "$(Get-TimeStamp): Requesting soft reboot and exit"
-        Exit 3010
+    if (!$LockscreenIMG -and !$WallpaperIMG){
+        LogWrite "$(Get-TimeStamp): Niether the lockscreen or background have values set."
     }
+    else{
 
-	If ($Mode -eq "Uninstall") {
-        LogWrite "$(Get-TimeStamp):Deleting the images"
-        Remove-Item "$Env:SystemDrive\Background\$NewBackgroundName\$NewBackgroundName" -Force -ErrorAction SilentlyContinue
-        Remove-Item "$Env:SystemDrive\Background\$NewBackgroundName\$NewLockScreenName" -Force -ErrorAction SilentlyContinue
-        LogWrite "$(Get-TimeStamp): Images Deleted"
-
-        LogWrite "$(Get-TimeStamp): Deleting registry entries"
-        Remove-ItemProperty -Path $RegistryKeyPath -Name $LockScreenImageStatus -Force -ErrorAction SilentlyContinue
-        Remove-ItemProperty -Path $RegistryKeyPath -Name $LockScreenImagePath -Force -ErrorAction SilentlyContinue
-        Remove-ItemProperty -Path $RegistryKeyPath -Name $LockScreenImageUrl -Force -ErrorAction SilentlyContinue
-        Remove-ItemProperty -Path $RegistryKeyPath -Name $DesktopImageStatus -Force -ErrorAction SilentlyContinue
-        Remove-ItemProperty -Path $RegistryKeyPath -Name $DesktopImagePath -Force -ErrorAction SilentlyContinue
-        Remove-ItemProperty -Path $RegistryKeyPath -Name $DesktopImageUrl -Force  -ErrorAction SilentlyContinue
-        LogWrite "$(Get-TimeStamp): Registry entries deleted"
-        LogWrite "$(Get-TimeStamp): Requesting soft reboot and exit"
-        Exit 3010
+        if ($LockscreenIMG){
+            LogWrite "$(Get-TimeStamp): Copying lockscreen ""$($LockscreenIMG)"" to ""$($LockscreenLocation)"""
+            Copy-Item ".\Assets\$LockscreenIMG" $LockscreenLocation -Force
+            LogWrite "$(Get-TimeStamp): Creating regkeys for lockscreen"
+            New-ItemProperty -Path $RegistryKeyPath -Name $LockScreenStatus -Value $StatusValue -PropertyType DWORD -Force
+            New-ItemProperty -Path $RegistryKeyPath -Name $LockScreenPath -Value $LockscreenLocation -PropertyType STRING -Force
+            New-ItemProperty -Path $RegistryKeyPath -Name $LockScreenUrl -Value $LockscreenLocation -PropertyType STRING -Force
+        }
+        if ($WallpaperIMG){
+            LogWrite "$(Get-TimeStamp): Copying wallpaper ""$($WallpaperIMG)"" to ""$($BackgroundLocation)"""
+            Copy-Item ".\Assets\$WallpaperIMG" $BackgroundLocation -Force
+            LogWrite "$(Get-TimeStamp): Creating regkeys for wallpaper"
+            New-ItemProperty -Path $RegistryKeyPath -Name $BackgroundStatus -Value $StatusValue -PropertyType DWORD -Force
+            New-ItemProperty -Path $RegistryKeyPath -Name $BackgroundPath -Value $BackgroundLocation -PropertyType STRING -Force
+            New-ItemProperty -Path $RegistryKeyPath -Name $DesktopUrl -Value $BackgroundLocation -PropertyType STRING -Force
+        }  
     }
+    LogWrite "$(Get-TimeStamp): Script has been completed."
+    exit
+}
+
+If ($Mode -eq "Uninstall") {
+    if (!$LockscreenIMG -and !$WallpaperIMG){
+    LogWrite "$(Get-TimeStamp): Niether the lockscreen or background have values set."
+}
+else{
+        if ($LockscreenIMG){
+            LogWrite "$(Get-TimeStamp): Removing the registry keys and file for the lockscreen"
+            Remove-ItemProperty -Path $RegistryKeyPath -Name $LockScreenStatus -Force
+            Remove-ItemProperty -Path $RegistryKeyPath -Name $LockScreenPath -Force
+            Remove-ItemProperty -Path $RegistryKeyPath -Name $LockScreenUrl -Force
+            Remove-Item $LockscreenLocation -Force
+        }
+        if ($WallpaperIMG){
+            LogWrite "$(Get-TimeStamp): Removing the registry keys and file for the background"
+            Remove-ItemProperty -Path $RegistryKeyPath -Name $BackgroundStatus -Force
+            Remove-ItemProperty -Path $RegistryKeyPath -Name $BackgroundPath -Force
+            Remove-ItemProperty -Path $RegistryKeyPath -Name $DesktopUrl -Force
+            Remove-Item $BackgroundLocation -Force
+        }  
+    }
+    LogWrite "$(Get-TimeStamp): Script has been completed."
+    exit
+}
